b28ba546507c13536b68cae0cdd4f6419f22086f
babu-ch <mashirohira@gmail.com>
2023-01-15 18:25:13 +0900

    add test

--- test/VHtmlLinkHandler.vue.test.ts
+++ test/VHtmlLinkHandler.vue.test.ts
@@ -1,5 +1,37 @@
+import {mount} from "@vue/test-utils";
+import VHtmlLinkHandler from "../src/components/VHtmlLinkHandler.vue"
+
+const mockedPush = vi.fn()
+const mockedWindowOpen = vi.fn()
+
+vi.mock('vue-router', () => ({
+    useRouter: vi.fn(() => ({
+        push: mockedPush
+    }))
+}))
+
+vi.mock('window', () => ({
+    open: mockedWindowOpen,
+}))
+
+const getComponent = (props: any) :VHtmlLinkHandler => {
+    return mount(VHtmlLinkHandler, {
+        props,
+    })
+}
 
 describe("VHtmlLinkHandler", () => {
+    afterEach(() => {
+        vi.restoreAllMocks()
+    })
+    test("test", async () => {
+        const html = '<p><a href="/relative">relative</a></p>'
+        const component = getComponent({html})
+
+        // expect router-push
+        await component.get("a").trigger("click")
+        expect(mockedPush).toBeCalled()
+    })
 })
 
 export {}
