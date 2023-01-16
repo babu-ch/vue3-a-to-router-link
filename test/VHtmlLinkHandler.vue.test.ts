6bc224c0bc8730b2d88272cf2e0eb5ca2d0e2af5
babu-ch <mashirohira@gmail.com>
2023-01-16 21:44:23 +0900

    add test

--- test/VHtmlLinkHandler.vue.test.ts
+++ test/VHtmlLinkHandler.vue.test.ts
@@ -12,37 +12,97 @@ vi.mock('vue-router', () => ({
 
 window.open = mockedWindowOpen
 
-const getComponent = (props: any) :VHtmlLinkHandler => {
+const getComponent = (props: any): VHtmlLinkHandler => {
     return mount(VHtmlLinkHandler, {
         props,
     })
 }
 
+const relativeHtml = '<p><a href="/relative">relative</a></p>'
+const externalLink = "https://example.com/hello"
+const externalHtml = `<p><a href="${externalLink}">external</a></p>`
+
+// TODO emit
 describe("VHtmlLinkHandler", () => {
     afterEach(() => {
         vi.restoreAllMocks()
     })
-    test("relative link -> router-push", async () => {
-        const html = '<p><a href="/relative">relative</a></p>'
-        const component = getComponent({html})
+    describe("default", () => {
+        test("relative link -> router-push", async () => {
+            const component = getComponent({html: relativeHtml})
 
-        await component.get("a").trigger("click")
-        expect(mockedPush).toBeCalledWith("/relative")
+            await component.get("a").trigger("click")
+            expect(mockedPush).toBeCalledWith("/relative")
+        })
+        test("internal link -> router-push", async () => {
+            const html = `<p><a href="${window.location.origin}/relative">full path</a></p>`
+            const component = getComponent({html})
+
+            await component.get("a").trigger("click")
+            expect(mockedPush).toBeCalledWith("/relative")
+        })
+        test("external link -> window open(blank)", async () => {
+            const component = getComponent({html: externalHtml})
+
+            await component.get("a").trigger("click")
+            expect(mockedWindowOpen).toBeCalledWith(externalLink, "_blank")
+        })
     })
-    test("internal link -> router-push", async () => {
-        const html = `<p><a href="${window.location.origin}/relative">full path</a></p>`
-        const component = getComponent({html})
+    describe("callback", () => {
+        test("internal callback(true) -> router-push", async () => {
+            const component = getComponent({html: relativeHtml, beforeMoveInternalCallBack() { return true }})
 
-        await component.get("a").trigger("click")
-        expect(mockedPush).toBeCalledWith("/relative")
+            await component.get("a").trigger("click")
+            expect(mockedPush).toBeCalledWith("/relative")
+        })
+        test("internal callback(false) -> nope", async () => {
+            const component = getComponent({html: relativeHtml, beforeMoveInternalCallBack() { return false }})
+
+            await component.get("a").trigger("click")
+            expect(mockedPush).not.toBeCalled()
+        })
+        test("internal async callback(true) -> router-push", async () => {
+            const component = getComponent({html: relativeHtml, async beforeMoveInternalCallBack() { return true }})
+
+            await component.get("a").trigger("click")
+            expect(mockedPush).toBeCalledWith("/relative")
+        })
+        test("internal async callback(false) -> nope", async () => {
+            const component = getComponent({html: relativeHtml, async beforeMoveInternalCallBack() { return false }})
+
+            await component.get("a").trigger("click")
+            expect(mockedPush).not.toBeCalled()
+        })
+        test("external callback(true) -> window.open", async () => {
+            const component = getComponent({html: externalHtml, beforeMoveExternalCallBack() { return true }})
+
+            await component.get("a").trigger("click")
+            expect(mockedWindowOpen).toBeCalledWith(externalLink, "_blank")
+        })
+        test("external callback(false) -> nope", async () => {
+            const component = getComponent({html: externalHtml, beforeMoveExternalCallBack() { return false }})
+
+            await component.get("a").trigger("click")
+            expect(mockedWindowOpen).not.toBeCalled()
+        })
+        test("external async callback(true) -> window.open", async () => {
+            const component = getComponent({html: externalHtml, async beforeMoveExternalCallBack() { return true }})
+
+            await component.get("a").trigger("click")
+            expect(mockedWindowOpen).toBeCalledWith(externalLink, "_blank")
+        })
+        test("external async callback(false) -> nope", async () => {
+            const component = getComponent({html: externalHtml, async beforeMoveExternalCallBack() { return false }})
+
+            await component.get("a").trigger("click")
+            expect(mockedWindowOpen).not.toBeCalled()
+        })
     })
-    test("external link -> window open(blank)", async () => {
-        const link = "https://example.com/hello"
-        const html = `<p><a href="${link}">external</a></p>`
-        const component = getComponent({html})
-
-        await component.get("a").trigger("click")
-        expect(mockedWindowOpen).toBeCalledWith(link, "_blank")
+    describe("check blank", () => {
+        test("internal check blank", async () => {
+        })
+        test("external check blank", async () => {
+        })
     })
 })
 
