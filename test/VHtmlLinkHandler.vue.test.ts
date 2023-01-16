c2d565650ac038ad4fe9b4e5ad4a8a379849952f
babu-ch <mashirohira@gmail.com>
2023-01-16 21:28:25 +0900

    add test

--- test/VHtmlLinkHandler.vue.test.ts
+++ test/VHtmlLinkHandler.vue.test.ts
@@ -10,9 +10,7 @@ vi.mock('vue-router', () => ({
     }))
 }))
 
-vi.mock('window', () => ({
-    open: mockedWindowOpen,
-}))
+window.open = mockedWindowOpen
 
 const getComponent = (props: any) :VHtmlLinkHandler => {
     return mount(VHtmlLinkHandler, {
@@ -24,13 +22,27 @@ describe("VHtmlLinkHandler", () => {
     afterEach(() => {
         vi.restoreAllMocks()
     })
-    test("test", async () => {
+    test("relative link -> router-push", async () => {
         const html = '<p><a href="/relative">relative</a></p>'
         const component = getComponent({html})
 
-        // expect router-push
         await component.get("a").trigger("click")
-        expect(mockedPush).toBeCalled()
+        expect(mockedPush).toBeCalledWith("/relative")
+    })
+    test("internal link -> router-push", async () => {
+        const html = `<p><a href="${window.location.origin}/relative">full path</a></p>`
+        const component = getComponent({html})
+
+        await component.get("a").trigger("click")
+        expect(mockedPush).toBeCalledWith("/relative")
+    })
+    test("external link -> window open(blank)", async () => {
+        const link = "https://example.com/hello"
+        const html = `<p><a href="${link}">external</a></p>`
+        const component = getComponent({html})
+
+        await component.get("a").trigger("click")
+        expect(mockedWindowOpen).toBeCalledWith(link, "_blank")
     })
 })
 
